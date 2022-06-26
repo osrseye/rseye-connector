d5be99718e3c2b0be67903db865b6992b1711d24
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-26 17:51:56 +0100

    update plugin properties / gradle dependencies / formatting

--- src/test/java/com/rseye/PluginTest.java
+++ src/test/java/com/rseye/PluginTest.java
@@ -3,11 +3,9 @@ package com.rseye;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class PluginTest
-{
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(Plugin.class);
+public class PluginTest {
+	public static void main(String[] args) throws Exception {
+		ExternalPluginManager.loadBuiltin(ConnectorPlugin.class);
 		RuneLite.main(args);
 	}
 }
