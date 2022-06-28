f27c8b76f3f01c12c2bd0dd0dc551906f657f505
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-28 14:20:56 +0100

    update config layout / add tick frequency control

--- src/main/java/com/rseye/ConnectorConfig.java
+++ src/main/java/com/rseye/ConnectorConfig.java
@@ -1,106 +1,149 @@
 package com.rseye;
 
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.*;
 
 @ConfigGroup("rseye-connector")
 public interface ConnectorConfig extends Config {
+	@ConfigSection(
+			position = 0,
+			name = "Endpoint Configuration",
+			description = "Endpoint POST configuration"
+	)
+	String apiConfig = "apiConfig";
+
+	@ConfigSection(
+			position = 1,
+			name = "Data Toggles",
+			description = "Enable/Disable which data is sent"
+	)
+	String dataToggles = "dataToggles";
+
+	@ConfigSection(
+			position = 2,
+			name = "Data Frequency",
+			description = "Set the frequency that data is sent"
+	)
+	String dataFrequency = "dataFrequency";
+
 	@ConfigItem(
+			position = 0,
 			keyName = "Base Endpoint",
 			name = "Endpoint",
-			description = "Endpoint to send data to: (example: http://localhost/api/v1/)"
+			description = "Endpoint to send data to: (example: http://localhost/api/v1/)",
+			section = apiConfig
 	)
 	default String baseEndpoint() {
 		return "http://localhost/api/v1/";
 	}
 
 	@ConfigItem(
-		keyName = "Bearer Token",
-		name = "bearerToken",
-		description = "Token provided to endpoint"
+			position = 1,
+			keyName = "Bearer Token",
+			name = "bearerToken",
+			description = "Token provided to endpoint",
+			section = apiConfig
 	)
 	default String bearerToken() {
 		return "token";
 	}
 
 	@ConfigItem(
-		position = 1,
-		keyName = "Send Position Data",
-		name = "Position Data",
-		description = "Toggle to send/omit player position data"
+			position = 0,
+			keyName = "Send Position Data",
+			name = "Position Data",
+			description = "Toggle to send/omit player position data",
+			section = dataToggles
 	)
 	default boolean positionData() {
 		return true;
 	}
 
 	@ConfigItem(
-			position = 2,
+			position = 1,
 			keyName = "Send Login Data",
 			name = "Login Data",
-			description = "Toggle to send/omit login state data"
+			description = "Toggle to send/omit login state data",
+			section = dataToggles
 	)
 	default boolean loginData() {
 		return true;
 	}
 
 	@ConfigItem(
-			position = 3,
+			position = 2,
 			keyName = "Send Stat Data",
 			name = "Stat Data",
-			description = "Toggle to send/omit level-xp-boostedLevel data"
+			description = "Toggle to send/omit level-xp-boostedLevel data",
+			section = dataToggles
 	)
 	default boolean statsData() {
 		return true;
 	}
 
 	@ConfigItem(
-			position = 4,
+			position = 3,
 			keyName = "Send Quest Data",
 			name = "Quest Data",
-			description = "Toggle to send/omit quest data"
+			description = "Toggle to send/omit quest data",
+			section = dataToggles
 	)
 	default boolean questData() {
 		return true;
 	}
 
 	@ConfigItem(
-			position = 5,
+			position = 4,
 			keyName = "Send Bank Data",
 			name = "Bank Data",
-			description = "Toggle to send/omit bank data"
+			description = "Toggle to send/omit bank data",
+			section = dataToggles
 	)
 	default boolean bankData() {
 		return true;
 	}
 
 	@ConfigItem(
-			position = 6,
+			position = 5,
 			keyName = "Send Loot Data",
 			name = "Loot Data",
-			description = "Toggle to send/omit loot data"
+			description = "Toggle to send/omit loot data",
+			section = dataToggles
 	)
 	default boolean lootData() {
 		return true;
 	}
 
 	@ConfigItem(
-			position = 7,
+			position = 6,
 			keyName = "Send Inventory Data",
 			name = "Inventory Data",
-			description = "Toggle to send/omit inventory data"
+			description = "Toggle to send/omit inventory data",
+			section = dataToggles
 	)
 	default boolean inventoryData() {
 		return true;
 	}
 
 	@ConfigItem(
-			position = 8,
+			position = 7,
 			keyName = "Send Equipment Data",
 			name = "Equipment Data",
-			description = "Toggle to send/omit equipment data"
+			description = "Toggle to send/omit equipment data",
+			section = dataToggles
 	)
 	default boolean equipmentData() {
 		return true;
+	}
+
+	@ConfigItem(
+			position = 0,
+			keyName = "Position Data Frequency",
+			name = "Position Data",
+			description = "How often to send position data",
+			section = dataFrequency
+	)
+	@Units(Units.TICKS)
+	default int positionDataFrequency() {
+		return 1;
 	}
 }
